a89f2d8ed6fa76e6b482f7ddeb3a4a31d6fbb851
ipekyagmurkuzum <yagmurkuzuum@gmail.com>
2024-02-13 20:54:20 +0300

    crud methods for user endpoint are implemented

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,12 @@
+package api.endpoints;
+
+public class Routes {
+    public static String baseUrl = "https://petstore.swagger.io/v2";
+
+    //User Module
+    public static String postUrl = baseUrl + "/user";
+    public static String getUrl = baseUrl + "/user/{username}";
+    public static String deleteUrl = baseUrl + "/user/{username}";
+    public static String putUrl = baseUrl + "/user/{username}";
+
+}
---
+++ src/test/java/api/endpoints/UserEndpoint.java
@@ -0,0 +1,49 @@
+package api.endpoints;
+
+import api.payloads.User;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static api.endpoints.Routes.*;
+import static io.restassured.RestAssured.given;
+
+public class UserEndpoint {
+
+    public static Response createUser(User payload) {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post(postUrl);
+        return response;
+    }
+
+    public static Response readUser(String username) {
+        Response response = given()
+                .pathParam("username", username)
+                .when()
+                .get(getUrl);
+        return response;
+    }
+
+    public static Response updateUser(User payload, String username) {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(payload)
+                .when()
+                .put(putUrl);
+        return response;
+    }
+
+    public static Response deleteUser(String username) {
+        Response response = given()
+                .pathParam("username",username)
+                .when()
+                .delete(deleteUrl);
+        return response;
+    }
+
+
+}
